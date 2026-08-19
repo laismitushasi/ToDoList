c08cb91c1ee8dd3a26b661991c95144c56b952c1
Laís Carolina Mitushasi <lais.mitushasi@aluno.senai.br>
2026-08-19 10:59:45 -0300

    alterações aula 19/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -3,198 +3,91 @@ package todolist;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
+
 public class ToDoList {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
+        
+        String[] tarefas = new String[10];
         boolean[] concluidas = new boolean[10];
         
         int opcao = 0;
         
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
+        while(opcao != 5){
+            System.out.println("=======LISTA DE TAREFAS======");
+            System.out.println("1- Adicionar tarefa");
+            System.out.println("2- Listar tarefa");
+            System.out.println("3- Concluir tarefa");
+            System.out.println("4- Excluir tarefa");
+            System.out.println("5- Sair");  
             
-            System.out.print("escolha uma opcao");
+            System.out.println("Escolha uma opcao:");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
             
-        }
-    }
-    
-}package todolist;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
-        boolean[] concluidas = new boolean[10];
-        
-        int opcao = 0;
-        
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
-            
-            System.out.print("escolha uma opcao");
-            
-        }
-    }
-    
-}package todolist;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
-        boolean[] concluidas = new boolean[10];
-        
-        int opcao = 0;
-        
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
-            
-            System.out.print("escolha uma opcao");
+            switch(opcao){
+                case 1:
+                    int posicaoLivre = -1;
+                    
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i]== null){
+                            posicaoLivre = i;
+                            break;
+                            
+                        }
+                        
+                    }
+                    if (posicaoLivre == -1){
+                        System.out.println("A lista de tarefs esta cheia!");
+                        
+                    } else{
+                        System.out.println("Digite a tarefa:");
+                        tarefas[posicaoLivre]= scanner.nextLine();
+                        concluidas[posicaoLivre]= false;
+                        System.out.println("Tarefa adicionada com sucesso!");
+                    }
+                    
+                    break;
+                    
+                case 2:
+                    System.out.println("=========MINHAS TAREFAS=======");
+                    boolean existeTarefa = false;
+                    
+                    for(int i = 0; i <tarefas.length; i++){
+                        if(tarefas[i]!= null){
+                            existeTarefa = true;
+                            
+                            if(concluidas[i] == true){
+                                System.out.println((i + 1)+ " - [x]" + tarefas[i]);
+                            } else{
+                                  System.out.println((i + 1)+ " - [ ]" + tarefas[i]);
+                            }
+                        }
+                    }
+                    
+                    if (existeTarefa == false){
+                        System.out.println("Nenhuma tarefa cadastrada!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Digite a tarefa que deseja concluir:");
+                    int numeroConcluir = scanner.nextInt();
+                    
+                    int indiceConcluir = numeroConcluir -1;
+                    
+                    if (indiceConcluir >= 0 && indiceConcluir < tarefas.length && tarefas[indiceConcluir] != null){
+                        
+                        concluidas[indiceConcluir] = true;
+                        
+                        System.out.println("Tarefa concluida com sucesso");
+                    }else{
+                        System.out.println("Tarefa inválida!");
+                    }
+            }
             
         }
-    }
+        
     
-}package todolist;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
-        boolean[] concluidas = new boolean[10];
-        
-        int opcao = 0;
-        
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
-            
-            System.out.print("escolha uma opcao");
-            
-        }
-    }
-    
-}package todolist;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
-        boolean[] concluidas = new boolean[10];
-        
-        int opcao = 0;
-        
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
-            
-            System.out.print("escolha uma opcao");
-            
-        }
-    }
-    
-}package todolist;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] tarefas = new string [10];
-        boolean[] concluidas = new boolean[10];
-        
-        int opcao = 0;
-        
-        while (opcao != 5) {
-            System.out.println("\n === LISTA DE TAREFAS === ");
-            System.out.println(" 1- adicionar tarefa ");
-            System.out.println(" 2- listar tarefa");
-            System.out.println(" 3- concluir tarefa");
-            System.out.println(" 4- excluir tarefa");
-            System.out.println(" 5- Sair");
-            
-            System.out.print("escolha uma opcao");
-            
-        }
-    }
-    
+}
 }
